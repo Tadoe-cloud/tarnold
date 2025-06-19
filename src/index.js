a34b4c234252c6d0e18c9d02c397d9d696295efd
Tadoe-cloud <tadiwanashededa@gmail.com>
2025-06-19 11:16:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-Function generatePoem(event){
-  event.preventDefault();
-
- new Typewriter('#poem', {
-  strings:"A wreathèd garland of deservèd praise",
+Function displayPoem(response){
+  new Typewriter('#poem', {
+  strings: response.data.answer
   autoStart: true,
    delay: 1,
    cursor: "",
 });
 }
+}
+Function generatePoem(event){
+  event.preventDefault();
+ let apiKey = "69d4501e3519toa844fc8e50131f90ba";
+  let prompt = "";
+  let context = "";
+  let apiURL = "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
+  axios.get(apiURL).then(displayPoem);
+ 
 let poemFormElement = document.querySelector("#poem-generator");
 poemFormElement.addEventListener("submit",generatePoem);
