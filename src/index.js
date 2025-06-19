f668335dbfb6ecfb87df71963b52829ad333e685
Tadoe-cloud <tadiwanashededa@gmail.com>
2025-06-19 11:37:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 Function displayPoem(response){
+
+  console.log("poem generated");
+  
   new Typewriter('#poem', {
   strings: response.data.answer
   autoStart: true,
@@ -6,14 +9,14 @@ Function displayPoem(response){
    cursor: "",
 });
 }
-}
 Function generatePoem(event){
   event.preventDefault();
  let apiKey = "69d4501e3519toa844fc8e50131f90ba";
-  let prompt = "";
+  let prompt = "Generate a poem about Flowers";
   let context = "";
   let apiURL = "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
+  console.log("generating poem");
   axios.get(apiURL).then(displayPoem);
- 
+}
 let poemFormElement = document.querySelector("#poem-generator");
 poemFormElement.addEventListener("submit",generatePoem);
