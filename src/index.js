8df9768e22e5f8cc8052bb8f074f29640921f129
Tadoe-cloud <tadiwanashededa@gmail.com>
2025-06-19 13:00:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-Function displayPoem(response){
-
-  console.log("poem generated");
+function displayPoem(response) {
+  console.log("Poem generated");
   
-  new Typewriter('#poem', {
-  strings: response.data.answer
-  autoStart: true,
-   delay: 1,
-   cursor: "",
-});
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: ""
+  });
 }
-Function generatePoem(event){
+
+function generatePoem(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
- let apiKey = "69d4501e3519toa844fc8e50131f90ba";
-  let prompt = 'User instructions: Generate a poem about ${instructionsInput.value}'; 
-  let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include the title to the Poem. Sign the poem with 'Tadoe'inside a strong> element at the end of the poem and not at the beginning ";
+  let apiKey = "69d4501e3519toa844fc8e50131f90ba";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include the title to the Poem. Sign the poem with 'Tadoe' inside a <strong> element at the end of the poem and not at the beginning";
+
   console.log("Generating poem");
-  console.log('Prompt: ${prompt}');
-  console.log('Context: ${context}');
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
+    prompt
+  )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
+
   axios.get(apiURL).then(displayPoem);
 }
+
 let poemFormElement = document.querySelector("#poem-generator");
-poemFormElement.addEventListener("submit",generatePoem);
+poemFormElement.addEventListener("submit", generatePoem);
+
